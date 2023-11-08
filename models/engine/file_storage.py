bfc7ba3465f932fe80130a7c807e199d141b53bf
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-11-08 23:51:22 +0100

    added methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,63 +1,42 @@
 #!/usr/bin/python3
-""" Module for class BaseModel """
+""" Module for class FileStorage """
 
-import datetime
-import uuid
+from models.base_model import BaseModel
+import json
+import os
 
 
-class BaseModel:
-    """ Defines all common attributes/methods for other classes """
+class FileStorage(BaseModel):
+    """
+    Defines a class FileStorage that:
+        serializes instances to a JSON file
+        deserializes JSON file to instances
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the attributes:
+    Private Class Attributes:
+    -------------------------
+    file_path (str): path to the JSON file
+    objects (dict): stores all objects
+    """
+    __file_path = "file.json"
+    __objects = {}
 
-        Parameters:
-        -----------
-        args: Non-keyworded arguments won't be used
-        kwargs: Each key of this dictionary is an attribute name
-                The key's value is the attribute's value
+    def all(self):
+        """ Returns the dictionary __objects """
+        return FileStorage.__objects
 
-        if kwargs is empty, the attributes below are created:
-        id (str): Unique id assigned with uuid
-        created_at: assigned with time when an instance is created
-        updated_at: Assigned with time when an instance is created
-                    and updates when public instance method save()
-                    is called.
-        """
-        args = None
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "updated_at" or key == "created_at":
-                        value = datetime.datetime.fromisoformat(value)
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-
-    def __str__(self):
-        """ Returns a string representation of BaseModel """
-        return "[{}] ({}) {}"\
-            .format(__class__.__name__, self.id, self.__dict__)
+    def new(self, obj):
+        """ Adds a key to __objects """
+        key = __class__.__name__ + "." + self.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Updates the public instance attr @updated_at with the
-        current datetime
-        """
-        self.updated_at = datetime.datetime.now()
+        """ Serializes __objects to a json file specified in __file_path """
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(FileStorage.__objects))
 
-    def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values including
-        the __class__ key with the class name of the object """
-        a_dict = {}
-        a_dict["__class__"] = __class__.__name__
-        for key in self.__dict__:
-            value = getattr(self, key)
-            if key == "updated_at" or key == "created_at":
-                value = value.isoformat()
-            a_dict[key] = value
-        return a_dict
+    def reload(self):
+        """ Deserializes instances from a json file if path exists """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, encoding='utf-8') as f:
+                data = f.read()
+            FileStorage.__objects = json.loads(data)
