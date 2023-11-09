a10304af036c580f245c119991d149826d63a2fd
stephen_adah <mailstephenreal@gmail.com>
2023-11-09 03:44:56 -0800

    modified - 'from models.engine.file_storage import FileStorage'

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module creates a unique FileStorage instance for the application """
 
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
