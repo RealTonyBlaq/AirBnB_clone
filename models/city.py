4594ada3b3f67a21cfade63be2b2805fc78d20b0
stephen_adah <mailstephenreal@gmail.com>
2023-11-11 13:26:00 -0800

    models/city.py

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script of a class City"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """A class City that inherit from BaseModel"""
+
+    state_id = " "
+    name = " "
