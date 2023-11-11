951657ab0a965ac05c7c34ad6511d0204a2d6d04
stephen_adah <mailstephenreal@gmail.com>
2023-11-11 13:20:29 -0800

    models/state.py

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script of a class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """a class State that inherit from BaseModel"""
+
+    name = " "
