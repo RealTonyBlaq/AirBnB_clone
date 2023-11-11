61979372f4e0a51c75e96b59c62163d07e8c9634
stephen_adah <mailstephenreal@gmail.com>
2023-11-11 13:32:26 -0800

    Amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that defines the class Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity that inherit from BaseModel"""
+
+    name = " "
