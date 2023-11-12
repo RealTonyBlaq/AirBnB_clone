c51c865663f11aba14cdfb2bf3f0e651b0e83971
stephen_adah <mailstephenreal@gmail.com>
2023-11-12 14:52:28 -0800

    test/user class modification

--- models/user.py
+++ models/user.py
@@ -13,8 +13,22 @@ class User(BaseModel):
         first_name (str): User's first name
         last_name (str): User's last name
     """
+    def __init__(self, *args, **kwargs):
+        """initialize user Instance"""
+        super().__init__(*args, **kwargs)
+        self.email = " "
+        self.password = " "
+        self.first_name = " "
+        self.last_name = " "
 
-    email = " "
-    password = " "
-    first_name = " "
-    last_name = " "
+    def to_dict(self):
+        """Return dictionary representation of user"""
+        user_dict = super().to_dict()
+        user_dict.update({
+            'email': self.email,
+            'password': self.password,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            '__class__': 'User',
+        })
+        return user_dict
