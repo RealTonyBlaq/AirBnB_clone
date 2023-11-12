39a7ba82db3678b0b4a182e6afb33f9eeb6b5436
stephen_adah <mailstephenreal@gmail.com>
2023-11-11 23:25:37 -0800

    doctrings

--- models/place.py
+++ models/place.py
@@ -4,7 +4,21 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """class Place that inherit from BaseModel"""
+    """class Place that inherit from BaseModel
+
+    Attributes:
+        city_id (str): city id
+        user_id (str): The user id
+        name (str): name of the place
+        description (str): description of the place
+        number_rooms (int): The number of rooms
+        number_bathrooms (int): The number of bathrooms
+        max_guest (int): maximum number of guests
+        price_by_night (int): price per night
+        latitude (float): The latitude of the place
+        longitude (float): The longitude of the place
+        amenity_ids (list): list of amenity id
+    """
 
     city_id = " "
     user_id = " "
@@ -13,7 +27,7 @@ class Place(BaseModel):
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
-    price_by_nught = 0
+    price_by_night = 0
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
