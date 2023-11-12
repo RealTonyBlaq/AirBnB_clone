4c93fff8e518c1db51f155713fe65779dafa15d5
stephen_adah <mailstephenreal@gmail.com>
2023-11-12 15:08:03 -0800

    modified

--- models/user.py
+++ models/user.py
@@ -14,15 +14,21 @@ class User(BaseModel):
         last_name (str): User's last name
     """
     def __init__(self, *args, **kwargs):
-        """initialize user Instance"""
+        '''initialize user Instance'''
         super().__init__(*args, **kwargs)
         self.email = " "
         self.password = " "
         self.first_name = " "
         self.last_name = " "
 
+    """email = " "
+    password = " "
+    first_name = " "
+    last_name = " "
+    """
+
     def to_dict(self):
-        """Return dictionary representation of user"""
+        '''Return dictionary representation of user'''
         user_dict = super().to_dict()
         user_dict.update({
             'email': self.email,
